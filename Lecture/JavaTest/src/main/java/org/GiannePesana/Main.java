03587f714e395528e121120f38a9a83facf9fd72
Gianne-Pesana <gpesana@addu.edu.ph>
2025-03-22 13:35:17 +0800

    Changes locally

--- Lecture/JavaTest/src/main/java/org/GiannePesana/Main.java
+++ Lecture/JavaTest/src/main/java/org/GiannePesana/Main.java
@@ -3,5 +3,6 @@ package org.GiannePesana;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git test!!!!");
+        System.out.println("Changes in IDE locally");
     }
 }
