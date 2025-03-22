d3898b9846c1f27d52de67655c21ee2aaf087a2f
Gianne-Pesana <gpesana@addu.edu.ph>
2025-03-22 14:00:54 +0800

    Added Chicken.java

--- Lecture/JavaTest/src/main/java/org/GiannePesana/Chicken.java
+++ Lecture/JavaTest/src/main/java/org/GiannePesana/Chicken.java
@@ -1,4 +1,14 @@
 package org.GiannePesana;
 
-public class Chicken {
+public class Chicken extends Food {
+    String part;
+
+    public Chicken(String part) {
+        this.part = part;
+    }
+
+    public Chicken(String foodName, double calories, String part) {
+        super(foodName, calories);
+        this.part = part;
+    }
 }
