f7e27bc12d2ae69d682c635d404c27f86fb24a5f
Gianne-Pesana <gpesana@addu.edu.ph>
2025-03-22 13:48:03 +0800

    revert 2

--- Lecture/JavaTest/src/main/java/org/GiannePesana/Main.java
+++ Lecture/JavaTest/src/main/java/org/GiannePesana/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Git test!!!!");
         System.out.println("Changes in IDE locally");
         System.out.println("Testing git revert");
+        System.out.println("2");
     }
 }
