3b7e95c79bb30c61d31aeaa6fa9376e73d114516
Gianne-Pesana <gpesana@addu.edu.ph>
2025-03-22 13:43:14 +0800

    Revert "git revert test"
    This reverts commit 4139039897d7610a72db9e12279abd38b9cab04b.

--- Lecture/JavaTest/src/main/java/org/GiannePesana/Main.java
+++ Lecture/JavaTest/src/main/java/org/GiannePesana/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git test!!!!");
         System.out.println("Changes in IDE locally");
-        System.out.println("Testing git revert");
     }
 }
